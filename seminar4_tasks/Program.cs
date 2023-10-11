a7776f5dbed6435e0de78983285ec1399d3073ed
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-11 22:42:18 +0300

    add seminar 4 tasks

---
+++ seminar4_tasks/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача про третью цифру!
+// Console.Write("Insert the number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n < 100)
+//     Console.WriteLine("There is no third figure");
+// else
+// {
+//     while(n > 999)
+//         n = n / 10;          // delete numbers from the end till the third number
+//     Console.WriteLine(n % 10);
+// }
+//..........................................................................................................
+
