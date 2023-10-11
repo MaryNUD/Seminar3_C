0e4a202c13bbc775691e8ad907dc8bac573de674
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-11 22:22:09 +0300

    add autotest HW

---
+++ seminar3_HW/Program.cs
@@ -0,0 +1,36 @@
+﻿// Console.Write("Insert the number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n >= 10000 && n < 100000)
+// {
+//     if (n / 10000 == n % 10 && (n / 1000) % 10 == (n % 100) / 10)
+//     Console.WriteLine("True");
+//     else 
+//     Console.WriteLine("False");
+// }
+// else 
+// Console.WriteLine("Error");
+
+//...............................................................................................................
+
+// Console.Write("Insert x1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Insert y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Insert x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Insert y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Insert x3: ");
+// int x3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Insert y3: ");
+// int y3 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(y3 - x3, 2)), 2);
+// Console.WriteLine(d);
+//..........................................................................................................
+
+// Console.Write("Insert number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= n; i ++)
+//     Console.Write($"{Math.Pow(i, 3)} ");
